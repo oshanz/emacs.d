469299e8651bf4c5e83556d012e9ef6c1a49ac6a
Oshan Wisumperuma <warunaoshan@gmail.com>
2020-04-04 11:24:05 +0530

    cider update

---
+++ elpa/cider-hydra-20190816.1121/cider-hydra-autoloads.el
@@ -0,0 +1,22 @@
+;;; cider-hydra-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "cider-hydra" "cider-hydra.el" (24200 8367
+;;;;;;  794585 577000))
+;;; Generated autoloads from cider-hydra.el
+
+(autoload 'cider-hydra-mode "cider-hydra" "\
+Hydras for CIDER.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cider-hydra-autoloads.el ends here
